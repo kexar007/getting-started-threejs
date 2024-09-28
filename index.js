ffcb08f3f2ef10c07352bf79196ae576adb35e23
kexar007 <kexar007@gmail.com>
2024-09-28 18:45:19 +0530

    input code for torus

--- index.js
+++ index.js
@@ -20,3 +20,16 @@ camera.position.z= 3;
 const scene = new THREE.Scene()
 
 renderer.render(scene,camera)
+
+const geometry = new THREE.TorusGeometry(10,3,16,100)
+const material = new THREE.MeshBasicMaterial({color:0xFF6347,wireframe:true});
+const torus = new THREE.Mesh(geometry,material);
+scene.add(torus);
+/* renderer.render(scene,camera); */
+
+function animate(){
+  requestAnimationFrame(animate);
+  renderer.render(scene,camera);
+}
+
+animate()
