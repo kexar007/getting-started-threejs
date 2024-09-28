904017874594464f58e8e9b82c3c8c0e43f17bda
kexar007 <kexar007@gmail.com>
2024-09-29 01:15:35 +0530

    add orbit control and grid

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import * as THREE from 'three';
+import { OrbitControls } from "jsm/controls/OrbitControls.js";
+
 
 
 const w = window.innerWidth;
@@ -12,7 +14,7 @@ document.body.appendChild(renderer.domElement)
 const fov=75;
 const aspect=w/h;
 const near=0.1;
-const far=100;
+const far=1000;
 const camera = new THREE.PerspectiveCamera(fov,aspect,near,far);
 camera.position.z= 40;
 
@@ -23,26 +25,30 @@ const scene = new THREE.Scene()
 
 const geometry = new THREE.TorusGeometry(10,3,16,100);
 const material = new THREE.MeshStandardMaterial({
-    color: 'gold', /* wireframe:true */
+    color: 'brown', /* wireframe:true */
 });
 
 const torus = new THREE.Mesh(geometry,material);
 scene.add(torus);
 
 const pointLight = new THREE.PointLight(0xffffff);
-pointLight.position.set(25,25,25);
+pointLight.position.set(10,20,5);
 const ambientLight = new THREE.AmbientLight(0xffffff);
+const pointLightHelper = new THREE.PointLightHelper(pointLight);
+scene.add(pointLight,ambientLight,pointLightHelper);
 
+const gridhelper = new THREE.GridHelper(100,50);
+scene.add(gridhelper);
 
-scene.add(pointLight,ambientLight);
-
+const controls= new OrbitControls(camera,renderer.domElement);
 
 
 function animate(){
   requestAnimationFrame(animate);
-    torus.rotation.x+= 0.02;
-    torus.rotation.y+= 0.001;
-    torus.rotation.z+= 0.003;
+    torus.rotation.x+= 0.01;
+    torus.rotation.y+= 0.03;
+   // torus.rotation.z+= 0.003;
+   //controls.update();
 
 
 
