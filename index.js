eb3cb7c7a06d8dc602bd546407d7bdff344e64ea
kexar007 <kexar007@gmail.com>
2024-09-28 23:59:57 +0530

    add torus geometry and lighting setup

--- index.js
+++ index.js
@@ -9,27 +9,45 @@ document.body.appendChild(renderer.domElement)
 
 
 
-fov=75;
-aspect=w/h;
-near=0.1;
-far=1000;
+const fov=75;
+const aspect=w/h;
+const near=0.1;
+const far=100;
 const camera = new THREE.PerspectiveCamera(fov,aspect,near,far);
-camera.position.z= 3;
+camera.position.z= 40;
 
 
-const scene = new THREE.Scene()
+const scene = new THREE.Scene() 
 
-renderer.render(scene,camera)
 
-const geometry = new THREE.TorusGeometry(10,3,16,100)
-const material = new THREE.MeshBasicMaterial({color:0xFF6347,wireframe:true});
+
+const geometry = new THREE.TorusGeometry(10,3,16,100);
+const material = new THREE.MeshStandardMaterial({
+    color: 'gold', /* wireframe:true */
+});
+
 const torus = new THREE.Mesh(geometry,material);
 scene.add(torus);
-/* renderer.render(scene,camera); */
+
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(25,25,25);
+const ambientLight = new THREE.AmbientLight(0xffffff);
+
+
+scene.add(pointLight,ambientLight);
+
+
 
 function animate(){
   requestAnimationFrame(animate);
+    torus.rotation.x+= 0.02;
+    torus.rotation.y+= 0.001;
+    torus.rotation.z+= 0.003;
+
+
+
+
   renderer.render(scene,camera);
 }
 
-animate()
+animate() 
