b26cafb85560a158e7d52e270fb3aa7c3f6275d8
kexar007 <kexar007@gmail.com>
2024-09-28 18:41:32 +0530

    write coerrect codes

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
-import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.118/build/three.module.js";
+import * as THREE from 'three';
+
 
 const w = window.innerWidth;
 const h = window.innerHeight;
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 1000);
-camera.position.z = 5;
-const renderer = new THREE.WebGLRenderer({ antiAlias: true });
-renderer.setSize(w, h);
-document.body.appendChild(renderer.domElement);
+const renderer = new THREE.WebGLRenderer({antialias:true});
+renderer.setSize(w,h);
+document.body.appendChild(renderer.domElement)
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0xFFFF00 });
 
-const box = new THREE.Mesh(geometry, material);
-scene.add(box);
 
-function animate() {
-  requestAnimationFrame(animate);
-  box.rotation.y += 0.02;
-  box.rotation.z += 0.01;
-  renderer.render(scene, camera);
-}
+fov=75;
+aspect=w/h;
+near=0.1;
+far=1000;
+const camera = new THREE.PerspectiveCamera(fov,aspect,near,far);
+camera.position.z= 3;
 
-animate();
+
+const scene = new THREE.Scene()
+
+renderer.render(scene,camera)
